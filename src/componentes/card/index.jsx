d5f9052ead98e3306260104c579de10b9b3885d7
Fizada9 <138830078+Fizada9@users.noreply.github.com>
2023-09-19 18:37:14 -0300

    Add files via upload

---
+++ src/componentes/card/index.jsx
@@ -0,0 +1,15 @@
+import style from ''
+
+function Card({ name, fundacao, criador, local, logo}) {
+
+    return(
+        <section className={StyleSheet.Card}>
+        <h3>{name}</h3>
+        <p>{fundacao}</p>
+        <p>{criador}</p>
+        <p>{local}</p>
+        <img src={logo} alt="" style={{width: '250px', height: '100px'}}/>
+        </section>
+    )
+}
+export default Card
